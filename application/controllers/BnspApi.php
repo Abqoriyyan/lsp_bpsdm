b2c737f1c956d7237ae9a68bf406d42c42fec929
Abqoriyyan <yalmakjepara@gmail.com>
2026-09-03 21:52:03 +0700

    perbaikan keamanan-SAST

--- application/controllers/BnspApi.php
+++ application/controllers/BnspApi.php
@@ -35,17 +35,27 @@ class BnspApi
 
 		curl_setopt($ch, CURLOPT_COOKIEJAR, $this->_phpsess);
 		curl_setopt($ch, CURLOPT_COOKIEFILE, $this->_phpsess);
-		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1');
+		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36');
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+		curl_setopt($ch, CURLOPT_CAINFO, APPPATH . 'libraries/cacert.pem');
+
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_URL, $this->_urlbnspapi . $lib);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		curl_setopt($ch, CURLOPT_URL, $this->_urlbnspapi . $lib);
+
+		if (!empty($post)) {
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		}
 
 		$hasilurl = curl_exec($ch);
-		$info = json_decode($hasilurl, true);
+
+		if (curl_errno($ch)) {
+			$error_msg = curl_error($ch);
+			log_message('error', 'CURL Error: ' . $error_msg);
+			$hasilurl = json_encode(['error' => true, 'message' => 'Koneksi ke API gagal.']);
+		}
 
 		curl_close($ch);
 		return $hasilurl;
